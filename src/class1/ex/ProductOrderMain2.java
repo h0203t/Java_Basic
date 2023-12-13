0e8a9c4929f0d64b2acd21a6b6c9bf92dc03a86e
hyukeh <rnjsgurxo12345@gmail.com>
2023-12-13 13:11:32 +0900

    문제와 풀이

--- src/class1/ex/ProductOrderMain2.java
+++ src/class1/ex/ProductOrderMain2.java
@@ -3,25 +3,36 @@ package class1.ex;
 public class ProductOrderMain2 {
 
     public static void main(String[] args) {
+        ProductOrder[] orders = new ProductOrder[3];
+        orders[0] = createOrder("두부", 2000, 2);
+        orders[1] = createOrder("김치", 5000, 1);
+        orders[2] = createOrder("콜라", 1500, 2);
+
+        printOrders(orders);
+        int totalAmount = getTotalAmount(orders);
+        System.out.println("총 결제 금액 : " + totalAmount);
+    }
 
 
-        int totalAmount = 0;
+    public static ProductOrder createOrder(String productName, int price, int quantity){
+        ProductOrder order = new ProductOrder();
+        order.productName = productName;
+        order.price = price;
+        order.quantity = quantity;
+        return order;
+    }
+
+    static void printOrders(ProductOrder[] orders){
         for (ProductOrder order : orders) {
             System.out.println("상품명: " + order.productName + " 가격: " + order.price + " 수량: " + order.quantity);
+        }
+    }
+
+    static int getTotalAmount(ProductOrder[] orders){
+        int totalAmount = 0;
+        for (ProductOrder order : orders) {
             totalAmount += order.quantity * order.price;
         }
-
-    }
-
-    public ProductOrder createOrder(String productName, int price, int quantity) {
-        ProductOrder productOrder = new ProductOrder();
-        productOrder.productName = productName;
-        productOrder.price = price;
-        productOrder.quantity = quantity;
-        return productOrder;
-    }
-
-    public void printOrder(ProductOrder productOrder){
-        System.out.println("");
+        return totalAmount;
     }
 }
