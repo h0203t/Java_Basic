7d08c15683cd761bc23d2d6fca6521773cc536e4
hyukeh <rnjsgurxo12345@gmail.com>
2023-12-28 17:42:05 +0900

    생성사 예시 수정

--- src/construct/ConstructMain1.java
+++ src/construct/ConstructMain1.java
@@ -3,5 +3,12 @@ package construct;
 public class ConstructMain1 {
     public static void main(String[] args) {
         MemberConstruct member1 = new MemberConstruct("user1", 19,50);
+        MemberConstruct member2 = new MemberConstruct("user2", 15, 30);
+
+        MemberConstruct[] members = {member1, member2};
+
+        for (MemberConstruct member : members) {
+            System.out.println("이름 : " + member.name + " 나이 : " + member.age + " 성적 : " + member.grade);
+        }
     }
 }
--- src/construct/MemberConstruct.java
+++ src/construct/MemberConstruct.java
@@ -6,7 +6,6 @@ public class MemberConstruct {
     int grade;
 
     MemberConstruct(String name, int age, int grade) {
-        System.out.println("생성자 호출 = " + name + age + grade );
         this.name = name;
         this.age = age;
         this.grade = grade;
