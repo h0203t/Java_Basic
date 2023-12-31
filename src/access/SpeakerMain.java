b22b5c283df87314b6b76fe550e03e41b88cf8b8
hyukeh <rnjsgurxo12345@gmail.com>
2023-12-31 22:40:55 +0900

    접근 제어자 이해1

---
+++ src/access/SpeakerMain.java
@@ -0,0 +1,21 @@
+package access;
+
+public class SpeakerMain {
+
+    public static void main(String[] args) {
+        Speaker speaker = new Speaker(10);
+        speaker.showVolume();
+        speaker.volumeUp();
+
+        speaker.showVolume();
+        speaker.volumeUp();
+
+        speaker.showVolume();
+        speaker.volumeUp();
+
+        //필드에 직접 접근
+        System.out.println("volume 필드 직접 접근 수정");
+        speaker.volume = 200;
+        speaker.showVolume();
+    }
+}
