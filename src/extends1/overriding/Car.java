c589fa5d2a61cce9636360e7146a7c9b9449544e
hyukeh <rnjsgurxo12345@gmail.com>
2024-01-21 21:51:49 +0900

    상속과 메서드 오버로딩

---
+++ src/extends1/overriding/Car.java
@@ -0,0 +1,13 @@
+package extends1.overriding;
+
+public class Car {
+
+    public void move() {
+        System.out.println("자동 차를 이동 합니다.");
+    }
+
+    //추가
+    public void  openDoor(){
+        System.out.println("차 문을 엽니다.");
+    }
+}
