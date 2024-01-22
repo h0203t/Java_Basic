1ce66ce32ce5c4d4bcfbb84c1bbb57d08873233d
hyukeh <rnjsgurxo12345@gmail.com>
2024-01-22 17:47:13 +0900

    다운캐스팅과 주의점

---
+++ src/poly/basic/CastingMain1.java
@@ -0,0 +1,15 @@
+package poly.basic;
+
+public class CastingMain1 {
+
+    public static void main(String[] args) {
+        // 부모 변수가 자식 인스턴스 참조(다형적 참조)
+        Parent poly = new Child();
+        //단 자삭의 기능은 호출할 수 없다.
+        //poly.childMethod()
+
+        //다운캐스팅(부모 타임 -> 자식 타임)
+        Child child = (Child) poly;
+        child.childMethod();
+    }
+}
---
+++ src/poly/basic/CastingMain5.java
@@ -0,0 +1,9 @@
+package poly.basic;
+
+public class CastingMain5 {
+
+    public static void main(String[] args) {
+        Parent parent1 = new Parent();
+        Parent parent2 = new Child();
+    }
+}
